1ce8935c8a83854c4b13a594e4b3f3d4f79d2e1e
BULAT IAKHIN <bulat_man@mail.ru>
2024-06-03 17:57:58 -0600

    gold-1

---
+++ models/example/gold__dbt_model.sql
@@ -0,0 +1,11 @@
+
+-- Use the `ref` function to select from other models
+
+-- select id, type, payload
+-- from {{ ref('first_dbt_model') }}
+-- limit 5
+
+
+
+select id, type, payload
+from dbt_paper_de.silver__dbt_model
