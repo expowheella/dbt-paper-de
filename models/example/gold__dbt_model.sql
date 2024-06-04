4464d697fcf7ca42b20806f686ff314d8a3635e0
BULAT IAKHIN <bulat_man@mail.ru>
2024-06-04 10:41:33 -0600

    minor gold

--- models/example/gold__dbt_model.sql
+++ models/example/gold__dbt_model.sql
@@ -1,2 +1,22 @@
-select id, type, payload
-from paper-de.dbt_paper_de.silver__dbt_model
+SELECT
+    deduped.message_id,
+    deduped.operation,
+    deduped.amount,
+    deduped.publish_time,
+    deduped.type
+FROM
+    (
+        SELECT
+            *,
+            ROW_NUMBER() OVER (
+                PARTITION BY type
+                ORDER BY
+                    publish_time DESC
+            ) AS row_num
+        FROM
+            paper-de.streaming.raw_events
+    ) AS deduped
+WHERE
+    row_num = 1
+ORDER BY
+    deduped.publish_time asc;
