b8c4635ceaa88d89ebc4c2119a78d6b128ab485d
BULAT IAKHIN <bulat_man@mail.ru>
2024-06-04 03:18:45 -0600

    minor fix2

--- models/example/silver__dbt_model.sql
+++ models/example/silver__dbt_model.sql
@@ -5,10 +5,10 @@ select
     pt.publish_time,
     et.type
 from
-    'paper-de.dbt_paper_de.operation' op
-    left join 'paper-de.dbt_paper_de.pub_time' pt on pt.message_id = op.message_id
-    left join 'paper-de.dbt_paper_de.event_type' et on et.message_id = op.message_id
-    left join 'paper-de.dbt_paper_de.amount am' on am.message_id = op.message_id
+    paper-de.dbt_paper_de.operation op
+    left join paper-de.dbt_paper_de.pub_time pt on pt.message_id = op.message_id
+    left join paper-de.dbt_paper_de.event_type et on et.message_id = op.message_id
+    left join paper-de.dbt_paper_de.amount am on am.message_id = op.message_id
 where
     amount is not null
 order by
