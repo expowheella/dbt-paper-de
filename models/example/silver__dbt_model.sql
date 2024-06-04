065fda5a000045e813914ba59351f082730b3cdb
BULAT IAKHIN <bulat_man@mail.ru>
2024-06-04 03:00:09 -0600

    silver

---
+++ models/example/silver__dbt_model.sql
@@ -0,0 +1,15 @@
+select
+    distinct op.message_id,
+    op.operation,
+    am.amount,
+    pt.publish_time,
+    et.type
+from
+    paper - de.dbt_paper_de.operation op
+    left join paper - de.dbt_paper_de.pub_time pt on pt.message_id = op.message_id
+    left join paper - de.dbt_paper_de.event_type et on et.message_id = op.message_id
+    left join paper - de.dbt_paper_de.amount am on am.message_id = op.message_id
+where
+    amount is not null
+order by
+    pt.publish_time
